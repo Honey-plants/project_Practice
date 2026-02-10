cabab637e8609648edd4a03f44cc2c15ffb763d8
binz <roaring94s@gmail.com>
2026-02-10 17:15:09 +0900

    final fetch

--- frontend/src/pages/menuscan/CameraUploadPage.jsx
+++ frontend/src/pages/menuscan/CameraUploadPage.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
+import PreviewPage from './PreviewPage';
 import { CameraService } from '../../components/camera/CameraService';
 
 const GUIDE_MESSAGE = {
@@ -172,20 +173,20 @@ export default function CameraUploadPage({ onCapture }) {
      Preview
      ========================= */
 
-  // if (previewStep) {
-  //   return (
-  //     <PreviewPage
-  //       file={capturedImage}
-  //       goBack={() => {
-  //         setCapturedImage(null);
-  //         setPreviewStep(false);
-  //         setStarted(false);
-  //         setGuide(null);
-  //         setCameraClosed(false);
-  //       }}
-  //     />
-  //   );
-  // }
+  if (previewStep) {
+    return (
+      <PreviewPage
+        file={capturedImage}
+        goBack={() => {
+          setCapturedImage(null);
+          setPreviewStep(false);
+          setStarted(false);
+          setGuide(null);
+          setCameraClosed(false);
+        }}
+      />
+    );
+  }
 
   /* =========================
      Render
